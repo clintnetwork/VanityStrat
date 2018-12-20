d9de004aaae9b160a02374b937b870b8851f8173
Clint.Network <me@clint.network>
2018-12-20 01:14:47 +0100

    Init

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using NBitcoin;
+using System;
+
+namespace VanityStrat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                //TODO: replace prefix with anything you want
+                Console.WriteLine(VanityGenerate("PREFIX"));
+            }
+        }
+
+        private static string VanityGenerate(string prefix)
+        {
+            Key privateKey;
+            while (true)
+            {
+                privateKey = new Key();
+                string address = privateKey.PubKey.GetAddress(Network.StratisMain).ToString();
+
+                if (address.ToUpper().StartsWith(prefix.ToUpper()))
+                {
+                    string privateKeyForDisplay = privateKey.GetWif(Network.StratisMain).ToWif();
+                    return privateKeyForDisplay + " " + address;
+                }
+            }
+        }
+    }
+}
